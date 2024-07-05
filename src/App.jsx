e6bea0a8acc04d469208e5e6717beb3fb4427ba6
Sanju Devadas <sanjudevadas11@gmail.com>
2024-07-05 11:55:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignupPage from './Pages/Signup'
+import './App.css'
+
+function App() {
+ 
+  return (
+   <div>
+  <Home/>
+  
+   </div>
+  )
+}
+
+export default App
